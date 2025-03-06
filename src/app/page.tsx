76d05bfec632d1de30b4da10b0e140eafe54a8b5
Vinicius Bussolo <vinicius.buss13@gmail.com>
2025-03-06 13:20:52 -0300

    feat: criado a primeira pagina para inserir o nome

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,21 @@
 export default function Home() {
-  return <h1>hello world</h1>;
+  return (
+    <main className="w-screen h-screen bg-gray-950 flex justify-center items-center">
+      <form className="bg-gray-800 w-96 p-6 rounded-lg ">
+        <input
+          type="text"
+          placeholder="Digite seu nome"
+          className="w-full p-2 mb-4 text-white placeholder-gray-500 rounded-md 
+          focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+
+        <button
+          type="submit"
+          className="w-full p-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 cursor-pointer"
+        >
+          Ver Cartas
+        </button>
+      </form>
+    </main>
+  );
 }
